ecc8bd8de5b10884ad4025adc5a1c464a0378a4f
falonso11 <alonsofernando16@gmail.com>
2022-01-11 11:20:53 -0300

    Finish

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -20,7 +20,7 @@ function mayoriaDeEdad(edad) {
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
   if (edad >= 18) {
-    return "Allow";
+    return "Allowed";
   }
   if (edad < 18) {
     return "Not allowed";
@@ -36,7 +36,7 @@ function conection(status) {
   if (status === 1) {
     return "Online"; 
   } else if (status === 2) {
-    return "Offline";
+    return "Away";
   } else {
     return "Offline";
   }
@@ -139,19 +139,22 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  if (num1 > num2 && num3) {
-    return 'Numero 1 es mayor y positivo';
-  } else if (num3 > num1 || num3 > num2) {
+  if(num1 < 0 || num2 < 0 || num3 < 0) {
+    return "Hay negativos";
+  }
+  else if(num1 === 0 || num2 === 0 || num3 === 0) {
+    return "Error";
+  }
+  else if(num1 > 0 && num1 > num2 && num1 > num3) {
+    return "Número 1 es mayor y positivo";
+  }
+  else if(num3 > num1 && num3 > num2) {
     return num3 + 1;
-  } else if (-num1, -num2, -num3){
-    return 'Hay negativos';
-  } else if (num1 === 0 || num2 === 0 || num3 === 0) {
-    return 'Error';
-  } else {
-    return false
+  }
+  else {
+    return false;
   }
 }
-
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
   // De lo contrario devuelve "falso"
